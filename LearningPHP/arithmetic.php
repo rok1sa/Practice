8929ae7bb245c8c041ffa5f3b17944395d56c03f
rok1sa <veligo20@gmail.com>
2023-09-04 20:22:56 -0700

    Update arithmetic.php

--- LearningPHP/arithmetic.php
+++ LearningPHP/arithmetic.php
@@ -1,3 +1,31 @@
 <?php
+    // Arithmetic operators
 
+    // $x = 10;
+    // $y = 2;
+    // $z = null;
+
+    // $z = $x + $y;
+    // $z = $x - $y;
+    // $z = $x * $y;
+    // $z = $x / $y;
+    // $z = $x ** $y;
+    // $z = $x % $y;
+    // echo $z;
+
+    //Increment/Decrement operators
+    
+    // $counter = 0;
+
+    // $counter-=3;
+    // echo $counter;
+
+    //Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+    // $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    // echo $total;
 ?>
