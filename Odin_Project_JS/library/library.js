4e4fb2635588d2bb3a3056987838572c39e98b17
rok1sa <veligo20@gmail.com>
2023-09-10 16:28:37 -0700

    Update library.js

--- Odin_Project_JS/library/library.js
+++ Odin_Project_JS/library/library.js
@@ -1,1 +1,17 @@
-alert('hello!');
+const theLibrary = [];
+
+function Book(title, author, pages, isRead){
+    this.title = title;
+    this. author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+}
+
+function addBookToLibrary(){
+}
+
+function displayBooks(theLibrary){
+    theLibrary.forEach(x => console.log(x));
+    call.Book();
+}
