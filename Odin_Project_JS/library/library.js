fb456ced5c85250e554310aec9dd5a3fe03ee056
rok1sa <veligo20@gmail.com>
2023-09-24 16:24:14 -0700

    Update library.js

--- Odin_Project_JS/library/library.js
+++ Odin_Project_JS/library/library.js
@@ -1,5 +1,14 @@
 const theLibrary = [];
 
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const isReadInput = document.getElementById("isRead");
+
+const messageBox = document.getElementById('msgBoxDisplay');
+
+const libraryInput = document.getElementById('libraryInput');
+
 function Book(title, author, pages, isRead){
     this.title = title;
     this. author = author;
@@ -8,12 +17,46 @@ function Book(title, author, pages, isRead){
 
 }
 
-function addBookToLibrary(){ 
+function insert(){
+    theLibrary.push(titleInput.value);
+    theLibrary.push(authorInput.value);
+    theLibrary.push(pagesInput.value);
+    theLibrary.push(isReadInput.value);
+
+    clearAndShow();
 }
 
-const book1 = new Book('Title', "Author", 255, false);
+function clearAndShow(){
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = 0;
+    isRead.value = "";
 
-theLibrary.push(book1);
-console.log(theLibrary);
+    messageBox.innerHTML = "";
 
-document.write(theLibrary);
+    messageBox.innerHTML += "Book: " + theLibrary.join('', '') + '<br/>'
+}
+
+function addBookToLibrary(){
+    //
+}
+
+insert();
+
+//const book1 = new Book('Crime and Punishment', "Dostoevsky", 255, false);
+
+//theLibrary.push(book1);
+
+/*
+document.getElementById('btnAddBook').onclick = function (){
+    const nHTML = '';
+    const userInput = prompt('Enter your book: ')
+
+    theLibrary.push(userInput);
+    theLibrary.forEach(function(item){
+        nHTML += '<li>' + item + '<li>';
+    });
+
+    document.getElementById('item-list').innerHTML = '<ul>' + nHTML + '<ul>'
+}
+*/
