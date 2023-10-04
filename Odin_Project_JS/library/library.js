eef61e10b5359eeff0c85a2c2db3364b5599e48e
rok1sa <veligo20@gmail.com>
2023-10-04 16:46:46 -0700

    Update library.js

--- Odin_Project_JS/library/library.js
+++ Odin_Project_JS/library/library.js
@@ -2,8 +2,11 @@ title = document.getElementById("title").value;
 author = document.getElementById("author").value;
 pages = document.getElementById('pages').value;
 isRead = document.getElementById('isRead').value;
-//sendBtn = document.getElementById("send");
+let container = document.getElementById('booksDiv'); 
 
+const theLibrary = [
+    
+];
 
 const Book= class{
     constructor(author, title, pages, isRead){
@@ -14,50 +17,77 @@ const Book= class{
     }
 }
 
-var dataSubmittingForm = document.getElementById("send").addEventListener("click", function(){
-
-    var newBook = new Book();
-    newBook.title = document.getElementById("title").value;    
-    newBook.author = document.getElementById("author").value;
-    newBook.pages = document.getElementById('pages').value;
-    newBook.isRead = document.getElementById('isRead').value;
-
-    const prevent = function(){
-        var form = document.querySelector("form").addEventListener("click", function(event){
-    
-            event.preventDefault();
-        })
-    }
-    
-    prevent();
-
-    console.log(newBook);
-    theLibrary.push(newBook);
-    console.log(theLibrary);
-
-})
-
-const theLibrary = [
-    
-];
-
-// there needs to be a func that will create new div for each book added and 
-// this func that creates divs also needs to assign id 
-// (or something to distinguish each div from each other)
-// to the div & assign values entered by user from a form.
-function addText(){
-    document.getElementById("book1Div").innerHTML = document.getElementById("title").value;
+const handleSubmit = function(){
+    let form = document.querySelector("form").addEventListener("click", function(event){
+        event.preventDefault();
+    });
 }
 
+/*var dataSubmittingForm = document.getElementById("send").addEventListener("click", function(){
+
+    //let newBook = {title: title, author: author, pages: pages, isRead: isRead};
+    //newBook.title = document.getElementById("title").value;    
+    //newBook.author = document.getElementById("author").value;
+    //newBook.pages = document.getElementById('pages').value;
+    //newBook.isRead = document.getElementById('isRead').value;
+
+    const prevent = function(){
+        let form = document.querySelector("form").addEventListener("click", function(event){
+            event.preventDefault();
+        });
+    }
+    prevent();
+
+    //console.log(newBook);
+    //theLibrary.push(newBook);
+    console.log(theLibrary);
+
+});*/
+
+var dataSubmittingForm = document.getElementById("sendBtn").addEventListener("click", function(){
+let newObject = {title: title, author: author, pages: pages, isRead: isRead};
+theLibrary.push(newObject);
+renderObjects();
+function renderObjects(){
+    let container = document.getElementById('booksDiv'); 
+    container.innerHTML ="";
+    theLibrary.forEach(function(object, index){
+        let div = document.createElement("div");
+        div.id = "object-" + index;
+        div.innerHTML = `Title: ${object.title} Author: ${object.author} Pages: ${object.pages} isRead: ${object.isRead}`;
+        container.appendChild(div);
+    });   
+}
+
+});
+
+
+document.querySelector('form').addEventListener('click', handleSubmit);
+
 /*
-function addText(){
+theLibrary.forEach(function(object,index){
+    let div = document.createElement("div");
+    div.id = "object-" + index;
+    div.innerHTML = `Title: ${object.title} Author: ${object.author} Pages: ${object.pages} isRead: ${object.isRead}`;
+    container.appendChild(div);
+});*/
 
-    //
-    const isSendClicked = document.getElementById("send").addEventListener("click", function(e){
-        div = document.createElement("div");
-        div.id ="Div1"
-        document.getElementById("Div1").innerHTML = document.getElementById('title').value;
-    })
 
-}*/
+
+/*
+const displayBook = document.getElementById("booksDiv");
+myLibrary.forEach(function(object){
+    let objectDiv = document.createElement("div");
+    objectDiv = "Title:" +object.title + "Author: " + object.author + "Pages: " + object.pages + "Is book read?: " + object.isRead;
+});
+*/
+
+/*
+const container = document.getElementById('booksDiv');
+    theLibrary.forEach(function(e){
+        container.append('<div>' + e + '</div>');
+        container.innerHTML+='<div> class="booksDiv></div';
+});
+*/
+
 
